7c185c9695e68b35b23c47fe44ecfe72b8e4f283
thalysson-nascimento <thalyssonwg.nascimento@gmail.com>
2025-03-20 19:11:33 -0300

    feat: included responsive menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
+} from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
@@ -13,9 +21,11 @@ const MaterialModule = [
   MatButtonModule,
   MatSidenavModule,
   MatCardModule,
+  MatListModule,
+  MatDividerModule,
 ];
 
-const CoreModule = [RouterOutlet, RouterModule];
+const CoreModule = [RouterOutlet, RouterModule, CommonModule];
 
 @Component({
   selector: 'app-root',
@@ -25,8 +35,17 @@ const CoreModule = [RouterOutlet, RouterModule];
 })
 export class AppComponent implements OnInit {
   title = 'app-post-message';
+  showFiller = false;
+  links = ['Home', 'Message'];
+  isMobile = window.innerWidth <= 480;
 
-  constructor() {}
+  constructor(private cdRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobile = event.target.innerWidth <= 480;
+    this.cdRef.detectChanges(); // Força a atualização do Angular
+  }
 }
